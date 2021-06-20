2b394eddc4cd61a923d797e80eb5cde39a11d494
NaveenKumarBollepalli <bollepallinaveen@gmail.com>
2021-06-20 20:47:59 +0530

    Removing un used code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { Container } from "react-bootstrap"
 import ResumeHeader from './components/ResumeHeader';
 import ResumeBody from './components/ResumeBody';
