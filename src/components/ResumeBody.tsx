02cce3bd486716e6189bc82d29a743e81786d11b
Naveen Kumar Bollepalli <naveenkumar.bollepalli@hexagonsi.com>
2021-06-22 16:11:19 +0530

    Update content

--- src/components/ResumeBody.tsx
+++ src/components/ResumeBody.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { Container, Row, Col } from "react-bootstrap"
 import "../css/ResumeBody.css";
 import DetailSection, { DetailContent } from './DetailSection';
+
 const mySkills = ["React Native",
     "ReactJS",
     "Redux",
     "Realm JS",
-    "HTML & CSS",
     "JavaScript",
-    "MongoDB",
+    "HTML & CSS",
     "Rabbit MQ",
-    "C# ASP.NET",
+    "MongoDB",
     "Node JS",
+    "C# ASP.NET",
     "Azure CI/CD",
     "Docker & Kubernetes"
 ]
+
 export default function ResumeBody(props: any) {
 
     return (
@@ -46,17 +48,21 @@ export default function ResumeBody(props: any) {
                 </DetailContent>
             </DetailSection>
             <DetailSection sectionText={"WORK EXPERIENCE"}>
-                <DetailContent headerText={" Hexagon Capability Center India - (Since Dec 2018)"}>
+                <DetailContent headerText={"Hexagon Capability Center India - (Since Dec 2018)"}>
                     <span>
-                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile</span> - Working on React Native, with Redux and RealmJS, FCM/RabbitMQ(MQTT) support, Background location tracking, Map Integration.
+                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile</span> - Working on React Native, with Redux and RealmJS. Added the following features support to the product - Firebase Messaging/RabbitMQ Messaging(MQTT), Background location tracking, Map Integration, RocketChat Messaging Integration and many more.
                     </span>
                     <br />
                     <span>
-                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile Server</span> - Working on ASP.NET Core 3.1 MVC (C#) with WebAPI powered with React, MongoDB(Replica Set Configured with X.509 certification), RabbitMQ(Messaging Queue Integration), Made the application Docker and Kubernetes deployable
+                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile Server</span> - Working on ASP.NET Core 3.1 MVC (C#) with WebAPI powered with React, Integrated High Availablity MongoDB(Replica Set Configured with X.509 certification), RabbitMQ(Messaging Queue Integration) and Added support for Docker and Kubernetes deployent
                     </span>
                     <br />
                     <span>
-                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile Administrator</span> - Worked on Java Spring Application In containerized environments.
+                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Dispatcher</span> - Added Support for RocketChat Messaging server to be able to deploy in Windows servers. Modified The Chat Server to expose underlying apis & database for functionality enhancments
+                    </span>
+                    <br />
+                    <span>
+                        <span className={"font-weight-bold text-muted"}>Product - </span><span className={"font-italic"}>OnCall Mobile Administrator</span> - Worked on Java Spring Application with micro services in containerized environment.
                     </span>
                     <br />
                 </DetailContent>
@@ -70,7 +76,7 @@ export default function ResumeBody(props: any) {
                     </span>
                     <br />
                     <span>
-                        <span className={"font-weight-bold text-muted"}>Client - </span><span className={"font-italic"}>TCS</span> In house Project - Worked on React Native, Redux, Realm JS, JavaScript (ES6) and Node.js.
+                        <span className={"font-weight-bold text-muted"}>Client - </span><span className={"font-italic"}>TCS</span> In house Project - Worked on React Native, Redux, Realm JS, JavaScript (ES6) and Node.js. Prototyped applications for WebRTC communications using PWA, Cordova & React Native
                     </span>
                     <br />
                 </DetailContent>
@@ -92,10 +98,22 @@ export default function ResumeBody(props: any) {
             </DetailSection>
             <DetailSection sectionText={"SKILLS"}>
                 <Row className={"mb-4 px-3"}>
-                    {mySkills.map((skill:string) => <Col md={3} key={skill} className={"font-weight-bold text-center"}><span className={"bg-light shadow-sm card border-0 mb-2"}>{skill}</span></Col>)}
+                    {mySkills.map((skill: string) => <Col md={3} key={skill} className={"font-weight-bold text-center"}><span className={"bg-light shadow-sm card border-0 mb-2"}>{skill}</span></Col>)}
                 </Row>
             </DetailSection>
             <DetailSection sectionText={"ADDITIVE SKILLS"}>
+                <DetailContent headerText={"Point of sale application - (Electron + React + MongoDB)"}>
+                    <span>
+                        A standalone Windows/Mac/Linux application to handle Point Of Sale operations, Integrated with EFTPOS, Till machines(Limited support) & POS Thermal Printers. User Interface and functionality developed in React and Mongo DB for Data Storage.
+                    </span>
+                    <br />
+                </DetailContent>
+                <DetailContent headerText={"A Hostable Resume"}>
+                    <span>
+                        The skills and experience mentioned in a resume are to breif on the major technoligies one has worked on. A developers work alot more than what he can present in a Resume. The goal of this Concept is to fill that gap. To provide a hostable resume template, which can be extracted either as a brief or can be navigated to full length detailed skill set.
+                    </span>
+                    <br />
+                </DetailContent>
                 <DetailContent headerText={"Home controlling device using Raspberry PI 3 Model B"}>
                     <span>
                         Working On a Smartphone Controlled Home automation Project, Raspberry PI as the control server using Node JS and Express JS. Actuators and relays controlled over GPIO of Raspberry PI. Cross platform mobile app for over the internet control, using React Native.
